f52a8c6761aa729795defa757639f630ad153f4c
mustafaalisaba <musta.ali.saba@gmail.com>
2022-11-13 13:21:19 -0600

    tests

--- hello.py
+++ hello.py
@@ -1,1 +1,12 @@
-print("Hello")
+""" from sklearn.ensemble import RandomForestClassifier
+clf = RandomForestClassifier(random_state=0) """
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0, 2 * np.pi, 200)
+y = np.sin(x)
+
+fig, ax = plt.subplots()
+ax.plot(x, y)
+plt.show()
